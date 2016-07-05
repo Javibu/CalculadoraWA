e39c16f043f004f306e3463f29dec08f5e7e0a1f
Javibu <javieralcivar_96@hotmail.com>
2016-07-05 14:00:20 -0500

    diseñoCalculadoraBasica

---
+++ CalculadoraWA/app/src/main/java/com/facci/calculadorawa/MainActivityWa.java
@@ -0,0 +1,13 @@
+package com.facci.calculadorawa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityWa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_wa);
+    }
+}
